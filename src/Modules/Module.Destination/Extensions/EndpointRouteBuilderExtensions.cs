e598365e45f74063be1a52f528c1aba773f02abf
dmkk3r <dominik.koerber.95@gmail.com>
2025-01-23 22:17:58 +0100

    Small ui adjustments

--- src/Modules/Module.Destination/Extensions/EndpointRouteBuilderExtensions.cs
+++ src/Modules/Module.Destination/Extensions/EndpointRouteBuilderExtensions.cs
@@ -9,8 +9,8 @@ public static class EndpointRouteBuilderExtensions
     public static IEndpointRouteBuilder MapDestinationModule(this IEndpointRouteBuilder builder)
     {
         builder.MapGet("/destinations/docker", DockerDestinationEndpoints.GetDestinations);
-        builder.MapGet("/destinations/docker/create", DockerDestinationEndpoints.GetDockerDestinationDialog);
-        builder.MapPost("/destinations/docker/create", DockerDestinationEndpoints.PostDockerDestinationDialog)
+        builder.MapGet("/destinations/docker/add", DockerDestinationEndpoints.GetDockerDestinationDialog);
+        builder.MapPost("/destinations/docker/add", DockerDestinationEndpoints.PostDockerDestinationDialog)
             .DisableAntiforgery();
         builder.MapPost("/destinations/docker/test", DockerDestinationEndpoints.TestDockerDestination);
         builder.MapPost("/destinations/docker/containers", DockerDestinationEndpoints.CreateDockerContainer);
