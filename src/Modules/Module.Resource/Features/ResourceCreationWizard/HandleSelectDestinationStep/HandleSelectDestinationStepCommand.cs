a42e85e8f6091839992e670d61aec419a6417a20
dmkk3r <dominik.koerber.95@gmail.com>
2025-02-11 23:09:33 +0100

    Added destination step handler

---
+++ src/Modules/Module.Resource/Features/ResourceCreationWizard/HandleSelectDestinationStep/HandleSelectDestinationStepCommand.cs
@@ -0,0 +1,10 @@
+﻿using Mediator;
+using Module.Resource.Ui.ResourceCreationWizard;
+
+namespace Module.Resource.Features.ResourceCreationWizard.HandleSelectDestinationStep;
+
+public class HandleSelectDestinationStepCommand : IRequest
+{
+    public Guid DestinationId { get; set; }
+    public required MetadataForm Metadata { get; set; }
+}
---
+++ src/Modules/Module.Resource/Features/ResourceCreationWizard/HandleSelectDestinationStep/HandleSelectDestinationStepCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using Mediator;
+using Modules.Shared.Interfaces;
+
+namespace Module.Resource.Features.ResourceCreationWizard.HandleSelectDestinationStep;
+
+public class HandleSelectDestinationStepCommandHandler : IRequestHandler<HandleSelectDestinationStepCommand>
+{
+    private readonly IPloydWebStore _ploydWebStore;
+
+    public HandleSelectDestinationStepCommandHandler(IPloydWebStore ploydWebStore)
+    {
+        _ploydWebStore = ploydWebStore;
+    }
+
+    public async ValueTask<Unit> Handle(HandleSelectDestinationStepCommand request, CancellationToken cancellationToken)
+    {
+        return Unit.Value;
+    }
+}
