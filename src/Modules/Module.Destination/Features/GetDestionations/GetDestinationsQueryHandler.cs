ed662d93912e103798eee05569304144fc63fa18
dmkk3r <dominik.koerber.95@gmail.com>
2025-02-14 23:54:21 +0100

    DestinationsQuery handler implementation

--- src/Modules/Module.Destination/Features/GetDestionations/GetDestinationsQueryHandler.cs
+++ src/Modules/Module.Destination/Features/GetDestionations/GetDestinationsQueryHandler.cs
@@ -1,10 +1,16 @@
+using Marten;
 using Mediator;
 using Module.Destination.Contract;
 
 namespace Module.Destination.Features.GetDestionations;
 
-public class GetDestinationsQueryHandler : IQueryHandler<GetDestinationsQuery, IReadOnlyList<Contract.Destination>>
+public class GetDestinationsQueryHandler(IDestinationStore store) : IQueryHandler<GetDestinationsQuery, IReadOnlyList<Contract.Destination>>
 {
-    public ValueTask<IReadOnlyList<Contract.Destination>> Handle(GetDestinationsQuery query, CancellationToken cancellationToken) =>
-        throw new NotImplementedException();
+    public async ValueTask<IReadOnlyList<Contract.Destination>> Handle(GetDestinationsQuery query,
+        CancellationToken cancellationToken)
+    {
+        await using IQuerySession session = store.QuerySession();
+
+        return await session.Query<Contract.Destination>().ToListAsync(cancellationToken);
+    }
 }
